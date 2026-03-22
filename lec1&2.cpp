10786332bfae6618873fbd53f4c5c32bd11fa4df
Devanshpatel350 <devanshpatel350@gmail.com>
2026-03-22 13:54:26 -0400

    Added Day1 code

---
+++ lec1&2.cpp
@@ -0,0 +1,18 @@
+//c++ tutorisld course
+/* c++ : fast program, more cortrol
+   system resources + memory mangement 
+   
+   high performance.     
+   updata on- 2022-c++11
+               2014-c++14
+               2017-c++17
+               basic structure a c++ program.*/
+
+               # include<iostream>
+               using namespace std;
+
+               int main()
+               {
+                  cout<<"hello world";
+                  return 0;
+               }
---
+++ lec3&4.cpp
@@ -0,0 +1,69 @@
+//variables in c++//
+/*variable scope $ data types
+a variable is a container to hold data
+variable can be of various type
+primarilybwe have these variable types in c++;
+1) int -{1,2,3}
+2)floot- (1.1,1.2,1.3)
+3)char-(A,S,D,F,S,S,)
+4)DOUBLE- (1.354,.......)
+5)blooean-y or n;
+
+
+
+int sum =34; means sum is an integer variable which holds value 34 in memorory 
+
+
+
+
+syntax for declaring variables in c++
+
+
+
+> Data type variable name value
+ex int a=4,b=6
+vased on scope, variable can be classified into two types;
+local variables 
+global variables 
+variable scope:- 
+scope of a vriable is the region in code wheere the existence fo variable is valid.
+based on scope we have local and global variables in c++
+basic nput out put in c++ 
+c++ come with libraries which helps usin perforing input out put. In c++ sequence of bytes corresponding to input and output aer commonly known a streams 
+Input stream :- direction of flow of bytes takes place from input device (for ex keyboard ) to the main memory. 
+Output stream:- direction of flow of bytes takes place from main memory ot the outut devic (for example display).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+List of Reserved keywords in c++ 
+This is a list of keywords reserved in c++ 
+Since they are used by the language itself these keywords are not abailable for re-definition or overloading
+In short, yor cannot create a variable with the names.
+There are two type of header files:-
+System header files:- It comed wth the compiler. 
+User defined header files:- It is witten by the programmet presentin the current directory. 
+
+
+
+
+
+
+*/
