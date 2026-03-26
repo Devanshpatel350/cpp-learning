31893d225c538c51244fafa51769e0370832d4ee
Devanshpatel350 <devanshpatel350@gmail.com>
2026-03-26 14:28:52 -0400

    Day 10: Practiced do-while loop in C++

---
+++ lec10.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+/*Loop in c++:
+   there are three types of loops in c++:
+   1. while loop
+   2.while loop 
+   3. do-while loop 
+
+*/   
+//for loop in c++
+/*int i=1;
+cout<<i;
+i++;
+//////// Syntax For for loop
+
+for(initialization; condition; updation)
+{
+loop body (C ++ code);
+}
+*/
+
+for (int i = 0;i < 20; i++)
+{
+cout<<i<<endl;
+}
+
+
+//whileloop in C++///
+//Synta:
+//while (condition)://
+//   {
+//    C++ statement;
+//   }
+//{
+   /* code */
+//}
+//////////printing 1 to 40 using while loop
+// int i=1;
+// while(i<=40){
+//    cout<<i<<endl;
+//    i++;
+// }
+
+
+
+/////example of infinite while loop
+
+// int i =1;
+// while (true)
+// {
+//    /* code */
+//    cout<<i<<endl;
+//    i++;
+// }
+
+
+// int i=1;
+// do{
+//    cout<<i<<endl;
+//    i++;
+// }while (false);
+// {
+//    /* code */
+// }
+
+
+return 0;
+
+
+
+
+}
