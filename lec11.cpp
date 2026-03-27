5cc4931ef097d5fd181a78878df8a6d651500149
Devanshpatel350 <devanshpatel350@gmail.com>
2026-03-27 13:53:10 -0400

    Day X: break and continue in C++

---
+++ lec11.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+// int main(){
+//    for (int i = 0; i < 4; i++)
+//    {
+//     /* code */
+//     cout<<i<<endl;
+//     if(i==2){
+//     break;
+//    }
+// }  
+
+//     return 0;
+// }
+int main(){
+   for (int i = 0; i < 4; i++)
+   {
+    /* code */
+    //cout<<i<<endl;
+    if(i==2){
+    continue;;
+   }
+   cout<<i<<endl;
+}  
+
+    return 0;
+}
